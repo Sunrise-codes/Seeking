86348d9ec538a39c13f4f82e0753c7a3558d30e2
drqzszn <2590346687@qq.com>
2023-07-13 16:14:59 +0800

    小修改

--- me/seeking/commands/MusicCommand.java
+++ me/seeking/commands/MusicCommand.java
@@ -1,5 +1,9 @@
 package me.seeking.commands;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 import java.io.*;
@@ -16,7 +20,7 @@ public class MusicCommand implements Command {
     public static Player player;
     @Override
     public boolean run(String[] args) {
-        if(args.length > 1) {
+        if(args.length == 3) {
             if (args[1].equals("playByName")) {
                 StringBuilder name = new StringBuilder();
                 if (args.length > 2) {
@@ -37,22 +41,49 @@ public class MusicCommand implements Command {
                 } catch (MalformedURLException e) {
                     PlayerUtil.tellPlayer("ERROR IN EXECUTE PLAY METHOD, MESSAGE:" + e.getMessage());
                 }
-            } else if (args[1].equals("stop")) {
-                if (player != null) {
-                    player.close();
-                    player = null;
-                } else
-                    PlayerUtil.tellPlayer("我操你妈的 播放器都没实例你stop你妈生命？");
-            } else
+            }else if (args[1].equals("search")) {
+                String name123 = args[2];
+                String url = "https://music.163.com/api/search/get/web?csrf_token=hlpretag=&hlposttag=&s=" + name123 + "&type=1&offset=0&total=true&limit=5";
+
+                try {
+                    String jsonResponse = sendGetRequest(url);
+                    JsonObject jsonObject = new Gson().fromJson(jsonResponse, JsonObject.class);
+                    JsonObject result = jsonObject.getAsJsonObject("result");
+                    JsonArray songs = result.getAsJsonArray("songs");
+
+                    for (JsonElement songElement : songs) {
+                        JsonObject song = songElement.getAsJsonObject();
+                        String name = song.get("name").getAsString();
+                        String id = song.get("id").getAsString();
+
+                        PlayerUtil.tellPlayer("Name: " + name + " ID: " + id);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else
                 return false;
-        }else
+        } else if (args.length == 2) {
+             if (args[1].equals("help")) {
+                PlayerUtil.tellPlayer("-music help 查看帮助\n-music stop停止播放\n-music play播放音乐\n-music search music_name 搜索音乐\n-music playByName music_name 播放名字对应的音乐\n-music playById music_id 播放id对应的音乐");
+            }
+             else if (args[1].equals("stop")) {
+                 if (player != null) {
+                     player.close();
+                     player = null;
+                 } else
+                     PlayerUtil.tellPlayer("我操你妈的 播放器都没实例你stop你妈生命？");
+             }
+        }
+        else
             return false;
         return true;
     }
 
     @Override
     public String usage() {
-        return "-play 播放 | -stop 停止播放";
+        return "-music help查看帮助";
     }
     //播放url音乐
     public static void play(String url) throws MalformedURLException {
@@ -85,6 +116,36 @@ public class MusicCommand implements Command {
                 }
             }
         }).start();
+    }
+    private static String sendGetRequest(String url) throws IOException {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        StringBuilder response = new StringBuilder();
+
+        try {
+            URL requestUrl = new URL(url);
+            connection = (HttpURLConnection) requestUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return response.toString();
     }
 
     public static String getUrl(String id) {
