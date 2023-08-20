cfc38a26766c43d049b58645ca40671deb166245
Thricons <BetaTestNMSL@163.com>
2023-08-20 21:39:06 +0800

    value fix

--- src/main/java/me/seeking/value/Value.kt
+++ src/main/java/me/seeking/value/Value.kt
@@ -8,13 +8,12 @@ import java.util.function.Supplier
 
 open class Value<V>(name: String, value: V, visitable: Supplier<Boolean>) {
     private lateinit var name: String
-    open var value: V
+    open var value: V = value
     private val visitable: Supplier<Boolean>
 
     init {
         this.name = name
         this.visitable = visitable
-        this.value = value
     }
 
 
