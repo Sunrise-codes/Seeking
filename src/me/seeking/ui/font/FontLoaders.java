82731406a8ce1c6df1cb87eef7b551f75e935ef5
root <BetaTestNMSL@163.com>
2024-02-03 14:35:49 +0000

    Add new font

--- src/me/seeking/ui/font/FontLoaders.java
+++ src/me/seeking/ui/font/FontLoaders.java
@@ -11,6 +11,7 @@ public class FontLoaders {
     public static FontRender font18;
     public static FontRender fontBig18;
     public static FontRender iconFont18;
+    public static FontRender pFont16;
 
     public static void init() throws Throwable {
         font14 = createByResource("normal.ttf",14f,true);
@@ -18,6 +19,7 @@ public class FontLoaders {
         font18 = createByResource("normal.ttf",18f,true);
         fontBig18 = createByResource("big.otf",18f,true);
         iconFont18 = createByResource("ico.ttf",18f,true);
+	pFont16 = createByResource("ProductSans-Regular.ttf",16f,true);
     }
 
     private static FontRender createByName(String fontName, int size, boolean antiAliasing) {
